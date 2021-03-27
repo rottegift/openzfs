ccffd23c6021af2e6e0d5ca5eafbc16e8e069dfc
Sean Doran <smd@use.net>
2021-03-27 22:06:33 +0000

    change KMEM quantum back
    this really does nothing, the macro hasn't been
    used since 2015, but the commentary is interestingy
    
    [21:59:14] ok so yeah new allocators
    [21:59:28] lundman should try with big quantum again maybe, now that we know it was the alignment
    [22:04:00]
    
    re: panic
    
    panic(cpu 4 caller 0xffffff8028d26998): "kfree: size 16777216 > kalloc_largest_allocated 16334848"@/AppleInternal/BuildRoot/Library/Caches/com.apple.xbs/Sources/xnu/xnu-6153.141.16/osfmk/kern/kalloc.c:868
    Backtrace (CPU 4), Frame : Return Address
    0xffffff8253d6bb60 : 0xffffff8028d1963d mach_kernel : _handle_debugger_trap + 0x49d
    0xffffff8253d6bbb0 : 0xffffff8028e53ae5 mach_kernel : _kdp_i386_trap + 0x155
    0xffffff8253d6bbf0 : 0xffffff8028e4566e mach_kernel : _kernel_trap + 0x4ee
    0xffffff8253d6bc40 : 0xffffff8028cbfa40 mach_kernel : _return_from_trap + 0xe0
    0xffffff8253d6bc60 : 0xffffff8028d18d07 mach_kernel : _DebuggerTrapWithState + 0x17
    0xffffff8253d6bd60 : 0xffffff8028d190f7 mach_kernel : _panic_trap_to_debugger + 0x227
    0xffffff8253d6bdb0 : 0xffffff80294c01cc mach_kernel : _panic + 0x54
    0xffffff8253d6be20 : 0xffffff8028d26998 mach_kernel : _kfree + 0x1b8
    0xffffff8253d6be70 : 0xffffff80293f0413 mach_kernel : _IOFree + 0x13
    0xffffff8253d6be90 : 0xffffff7fac7207de net.lundman.zfs : _osif_free + 0xe
    0xffffff8253d6beb0 : 0xffffff7fac72bf21 net.lundman.zfs : _xnu_free_throttled + 0xb1
    0xffffff8253d6bee0 : 0xffffff7fac72c7e7 net.lundman.zfs : _vmem_bucket_free + 0xd7
    0xffffff8253d6bf00 : 0xffffff7fac723da6 net.lundman.zfs : _taskq_thread + 0x246
    0xffffff8253d6bfa0 : 0xffffff8028cbf13e mach_kernel : _call_continuation + 0x2e
          Kernel Extensions in backtrace:
             net.lundman.zfs(2.0)[7ED3DF9A-EEEE-3E87-B8BB-3F0ABC0C7949]@0xffffff7fac3e7000->0xffffff7fadca3fff
                dependency: com.apple.iokit.IOStorageFamily(2.1)[95CC5A58-BF77-3690-9EEE-720B18B7C829]@0xffffff7fa9665000

--- include/os/macos/spl/sys/vmem.h
+++ include/os/macos/spl/sys/vmem.h
@@ -35,8 +35,8 @@ extern "C" {
 
 // Make sure IOMalloc uses kernel_map, and not kalloc.zones
 // This is from XNU kalloc_max and kalloc_kernmap_size
-// #define	KMEM_QUANTUM (PAGESIZE << 2) * 16 + PAGESIZE;
-#define	KMEM_QUANTUM PAGESIZE
+#define	KMEM_QUANTUM (PAGESIZE << 2) * 16 + PAGESIZE;
+//#define	KMEM_QUANTUM PAGESIZE
 
 	/*
 	 * Per-allocation flags

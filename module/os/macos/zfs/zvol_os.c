e46360ab489786dddb21e3ae89063775855a7310
Sean Doran <smd@use.net>
2021-04-05 05:05:06 +0100

    experimentally allow three more taskq_d

--- module/os/macos/zfs/zvol_os.c
+++ module/os/macos/zfs/zvol_os.c
@@ -1090,7 +1090,7 @@ zvol_init(void)
 	int threads = MIN(MAX(zvol_threads, 1), 1024);
 
 	zvol_taskq = taskq_create(ZVOL_DRIVER, threads, maxclsyspri-4,
-	    threads * 2, INT_MAX, TASKQ_PREPOPULATE | TASKQ_DYNAMIC);
+	    threads * 2, INT_MAX, TASKQ_PREPOPULATE | TASKQ_DYNAMIC | TASKQ_REALLY_DYNAMIC);
 	if (zvol_taskq == NULL) {
 		return (-ENOMEM);
 	}

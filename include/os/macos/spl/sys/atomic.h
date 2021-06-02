5db33105efa56c0f1be30a515984d330a26904d7
Sean Doran <smd@use.net>
2021-06-02 18:42:26 +0100

    pass 4: flesh out more types

--- include/os/macos/spl/sys/atomic.h
+++ include/os/macos/spl/sys/atomic.h
@@ -55,8 +55,12 @@ extern "C" {
 
 
 ATOMIC_INC(8, uint8_t)
+ATOMIC_INC(uchar, uchar_t)
 ATOMIC_INC(16, uint16_t)
+ATOMIC_INC(ushort, ushort_t)
 ATOMIC_INC(32, uint32_t)
+ATOMIC_INC(uint, uint_t)
+ATOMIC_INC(ulong, ulong_t)
 ATOMIC_INC(64, uint64_t)
 
 #define	ATOMIC_INC_NV(name, type)			    \
@@ -67,7 +71,13 @@ ATOMIC_INC(64, uint64_t)
 		__ATOMIC_SEQ_CST));			    \
 	    }
 
+ATOMIC_INC_NV(8, uint8_t)
+ATOMIC_INC_NV(uchar, uchar_t)
+ATOMIC_INC_NV(16, uint16_t)
+ATOMIC_INC_NV(ushort, ushort_t)
 ATOMIC_INC_NV(32, uint32_t)
+ATOMIC_INC_NV(uint, uint_t)
+ATOMIC_INC_NV(ulong, ulong_t)
 ATOMIC_INC_NV(64, uint64_t)
 
 /*
@@ -83,8 +93,12 @@ ATOMIC_INC_NV(64, uint64_t)
 	}
 
 ATOMIC_DEC(8, uint8_t)
+ATOMIC_DEC(uchar, uchar_t)
 ATOMIC_DEC(16, uint16_t)
+ATOMIC_DEC(ushort, ushort_t)
 ATOMIC_DEC(32, uint32_t)
+ATOMIC_DEC(uint, uint_t)
+ATOMIC_DEC(ulong, ulong_t)
 ATOMIC_DEC(64, uint64_t)
 
 #define	ATOMIC_DEC_NV(name, type)				\
@@ -95,7 +109,13 @@ ATOMIC_DEC(64, uint64_t)
 			__ATOMIC_SEQ_CST));			\
 	}
 
+ATOMIC_DEC_NV(8, uint8_t)
+ATOMIC_DEC_NV(uchar, uchar_t)
+ATOMIC_DEC_NV(16, uint16_t)
+ATOMIC_DEC_NV(ushort, ushort_t)
 ATOMIC_DEC_NV(32, uint32_t)
+ATOMIC_DEC_NV(uint, uint_t)
+ATOMIC_DEC_NV(ulong, ulong_t)
 ATOMIC_DEC_NV(64, uint64_t)
 
 /*
@@ -111,8 +131,12 @@ ATOMIC_DEC_NV(64, uint64_t)
         }
 
 ATOMIC_ADD(8, uint8_t, int8_t)
+ATOMIC_ADD(char, uchar_t, signed char)
 ATOMIC_ADD(16, uint16_t, int16_t)
+ATOMIC_ADD(short, ushort_t, short)
 ATOMIC_ADD(32, uint32_t, int32_t)
+ATOMIC_ADD(int, uint_t, int)
+ATOMIC_ADD(long, ulong_t, long)
 ATOMIC_ADD(64, uint64_t, int64_t)
 
 #define	ATOMIC_ADD_NV(name, type1, type2)			\
@@ -126,14 +150,17 @@ ATOMIC_ADD(64, uint64_t, int64_t)
 	}
 
 ATOMIC_ADD_NV(8, uint8_t, int8_t)
+ATOMIC_ADD_NV(char, uchar_t, signed char)
 ATOMIC_ADD_NV(16, uint16_t, int16_t)
+ATOMIC_ADD_NV(short, ushort_t, short)
 ATOMIC_ADD_NV(32, uint32_t, int32_t)
+ATOMIC_ADD_NV(int, uint_t, int)
+ATOMIC_ADD_NV(long, ulong_t, long)
 ATOMIC_ADD_NV(64, uint64_t, int64_t)
 
 /*
  * Subtract delta to target
  */
-
 
 #define	ATOMIC_SUB(name, type1, type2)					\
 	static inline							\
@@ -144,16 +171,32 @@ ATOMIC_ADD_NV(64, uint64_t, int64_t)
         }
 
 ATOMIC_SUB(8, uint8_t, int8_t)
+ATOMIC_SUB(char, uchar_t, signed char)
 ATOMIC_SUB(16, uint16_t, int16_t)
+ATOMIC_SUB(short, ushort_t, short)
 ATOMIC_SUB(32, uint32_t, int32_t)
+ATOMIC_SUB(int, uint_t, int)
+ATOMIC_SUB(long, ulong_t, long)
 ATOMIC_SUB(64, uint64_t, int64_t)
 
-static inline uint64_t
-atomic_sub_64_nv(volatile uint64_t *target, int64_t delta)
-{
-	return ((uint64_t)__atomic_sub_fetch((volatile int64_t *)
-		target, delta, __ATOMIC_SEQ_CST));
-}
+#define	ATOMIC_SUB_NV(name, type1, type2)				\
+	static inline							\
+	type1 atomic_sub_##name##_nv(volatile type1 *target,		\
+	    type2 bits)							\
+        {                                                               \
+		return ((type1) __atomic_sub_fetch(			\
+			(volatile type2 *)target,			\
+			bits, __ATOMIC_SEQ_CST));			\
+        }
+
+ATOMIC_SUB_NV(8, uint8_t, int8_t)
+ATOMIC_SUB_NV(char, uchar_t, signed char)
+ATOMIC_SUB_NV(16, uint16_t, int16_t)
+ATOMIC_SUB_NV(short, ushort_t, short)
+ATOMIC_SUB_NV(32, uint32_t, int32_t)
+ATOMIC_SUB_NV(int, uint_t, int)
+ATOMIC_SUB_NV(long, ulong_t, long)
+ATOMIC_SUB_NV(64, uint64_t, int64_t)
 
 /*
  * logical OR bits with target
@@ -167,9 +210,29 @@ atomic_sub_64_nv(volatile uint64_t *target, int64_t delta)
         }
 
 ATOMIC_OR(8, uint8_t)
+ATOMIC_OR(uchar, uchar_t)
 ATOMIC_OR(16, uint16_t)
+ATOMIC_OR(ushort, ushort_t)
 ATOMIC_OR(32, uint32_t)
+ATOMIC_OR(uint, uint_t)
+ATOMIC_OR(ulong, ulong_t)
 ATOMIC_OR(64, uint64_t)
+
+#define ATOMIC_OR_NV(name, type) \
+	static inline							\
+        type atomic_or_##name##_nv(volatile type *target, type bits)    \
+        {                                                               \
+                return (__atomic_or_fetch(target, bits, __ATOMIC_SEQ_CST)); \
+        }
+
+ATOMIC_OR_NV(8, uint8_t)
+ATOMIC_OR_NV(uchar, uchar_t)
+ATOMIC_OR_NV(16, uint16_t)
+ATOMIC_OR_NV(ushort, ushort_t)
+ATOMIC_OR_NV(32, uint32_t)
+ATOMIC_OR_NV(uint, uint_t)
+ATOMIC_OR_NV(ulong, ulong_t)
+ATOMIC_OR_NV(64, uint64_t)
 
 /*
  * logical AND bits with target
@@ -183,8 +246,12 @@ ATOMIC_OR(64, uint64_t)
         }
 
 ATOMIC_AND(8, uint8_t)
+ATOMIC_AND(uchar, uchar_t)
 ATOMIC_AND(16, uint16_t)
+ATOMIC_AND(ushort, ushort_t)
 ATOMIC_AND(32, uint32_t)
+ATOMIC_AND(uint, uint_t)
+ATOMIC_AND(ulong, ulong_t)
 ATOMIC_AND(64, uint64_t)
 
 /*
@@ -203,8 +270,12 @@ ATOMIC_AND(64, uint64_t)
         }
 
 ATOMIC_CAS(8, uint8_t)
+ATOMIC_CAS(uchar, uchar_t)
 ATOMIC_CAS(16, uint16_t)
+ATOMIC_CAS(ushort, ushort_t)
 ATOMIC_CAS(32, uint32_t)
+ATOMIC_CAS(uint, uint_t)
+ATOMIC_CAS(ulong, ulong_t)
 ATOMIC_CAS(64, uint64_t)
 
 #define ATOMIC_SWAP(name, type)						\
@@ -215,8 +286,12 @@ ATOMIC_CAS(64, uint64_t)
         }
 
 ATOMIC_SWAP(8, uint8_t)
+ATOMIC_SWAP(uchar, uchar_t)
 ATOMIC_SWAP(16, uint16_t)
+ATOMIC_SWAP(ushort, ushort_t)
 ATOMIC_SWAP(32, uint32_t)
+ATOMIC_SWAP(uint, uint_t)
+ATOMIC_SWAP(ulong, ulong_t)
 ATOMIC_SWAP(64, uint64_t)
 
 static inline void *
